603930da43a2d97b12bd9427bb5d9ec4911b7e59
Lev Kusanagi <levkusanagi@gmail.com>
2018-03-15 02:11:25 +0100

    Complete first working expert manager.

--- expertmanager/expertmanager.py
+++ expertmanager/expertmanager.py
@@ -2,17 +2,21 @@
 # more dapp agents on the iExec cloud.
 
 import argparse
+from subprocess import run
 
 parser = argparse.ArgumentParser()
 parser.add_argument('input_words', metavar='N', type=str, nargs='+')
 input = parser.parse_args()
 input_string = ' '.join(input.input_words)
 
+command1 = "iexec submit 'python3 /examples/nlp/dime_algo.py' --dapp 0x25412143e4c5392dea108c2834008985507410f2"
+command1 = command1.split(' ')
+command2 = "iexec submit 'python oneturn.py how are you' --dapp 0x2e785496d4f66BF1e2dDFba1a7755eb2EbF75d2C"
+command2 = command2.split(' ')
 # Pseudocode
 if input_string == 'En que piensa Cervantes?':
-    print('calling Cervantes')
-#   then iexec submit 'python3 /examples/nlp/dime_algo.py' --dapp 0x25412143e4c5392dea108c2834008985507410f2
-#   else: input goes to tinychat iexec submit 'python oneturn.py %inputstring --dapp 0x2e785496d4f66BF1e2dDFba1a7755eb2EbF75d2C</p>
+    print('Raising Cervantes from his grave...')
+    run(command1)
 else:
-    print('Message sent to Athina tinychat')
-
+    print('Waiting for Athina\'s response')
+    run(command2)
