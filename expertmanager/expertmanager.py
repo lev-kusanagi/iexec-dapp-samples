d22d8a91c5d83f39db36758bfe3aacd1daf42dee
Lev Kusanagi <levkusanagi@gmail.com>
2018-03-15 03:26:13 +0100

    Use cleaner syntax.

--- expertmanager/expertmanager.py
+++ expertmanager/expertmanager.py
@@ -9,10 +9,9 @@ parser.add_argument('input_words', metavar='N', type=str, nargs='+')
 input = parser.parse_args()
 input_string = ' '.join(input.input_words)
 
-command1 = "iexec submit 'python3 /examples/nlp/dime_algo.py' --dapp 0x25412143e4c5392dea108c2834008985507410f2"
-command1 = command1.split(' ')
-command2 = "iexec submit 'python oneturn.py how are you' --dapp 0x2e785496d4f66BF1e2dDFba1a7755eb2EbF75d2C"
-command2 = command2.split(' ')
+command1 = "iexec submit 'python3 /examples/nlp/dime_algo.py' --dapp 0x25412143e4c5392dea108c2834008985507410f2".split()
+command2 = "iexec submit 'python oneturn.py how are you' --dapp 0x2e785496d4f66BF1e2dDFba1a7755eb2EbF75d2C".split()
+
 # Pseudocode
 if input_string == 'En que piensa Cervantes?':
     print('Raising Cervantes from his grave...')
