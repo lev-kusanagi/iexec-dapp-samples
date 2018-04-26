50a01f59c37a788ed785c9c3edd050823f144ab1
Lev Kusanagi <levkusanagi@gmail.com>
2018-04-27 01:40:18 +0200

    Revert contract names to MyContract

--- tinychat2/contracts/Tinychat2.sol
+++ tinychat2/contracts/Tinychat2.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.4.11;
 import "iexec-oracle-contract/contracts/IexecOracleAPI.sol";
-contract Tinychat2 is IexecOracleAPI{
+contract MyContract is IexecOracleAPI{
 
     uint public constant DAPP_PRICE = 0;
-    string public constant DAPP_NAME = "Tinychat2";
+    string public constant DAPP_NAME = "init";
     
-    function Tinychat2 (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress,DAPP_PRICE,DAPP_NAME){
+    function MyContract (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress,DAPP_PRICE,DAPP_NAME){
 
     }
 
